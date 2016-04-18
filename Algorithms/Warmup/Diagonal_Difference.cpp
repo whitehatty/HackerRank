f04ef8002092b27425e4403fc1977f72ff4df533
Tomasini Marcello <whitehatty@gmail.com>
2016-04-17 20:09:08 -0400

    Diagonal Difference

---
+++ Algorithms/Warmup/Diagonal_Difference.cpp
@@ -0,0 +1,67 @@
+/**
+Given a square matrix of size NxN, calculate the absolute difference between the sums of its diagonals.
+
+Input Format
+
+The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line containing N space-separated integers describing the columns.
+
+Output Format
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+
+Sample Output
+
+15
+
+Explanation
+
+The primary diagonal is:
+11
+      5
+            -12
+
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+            4
+      5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+**/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector< vector<int> > a(n,vector<int>(n));
+    int sumd1 = 0;
+    int sumd2 = 0;
+    for(int a_i = 0;a_i < n;a_i++){
+       for(int a_j = 0;a_j < n;a_j++){
+          cin >> a[a_i][a_j];
+           if (a_i == a_j)
+               sumd1 += a[a_i][a_j];
+           if (a_j == n - a_i - 1){
+               sumd2 += a[a_i][a_j];
+           }
+       }
+    }
+    cout << abs(sumd1 - sumd2);
+    return 0;
+}
+
